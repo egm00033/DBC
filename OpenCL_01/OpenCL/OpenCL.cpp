04805253a6d1bb8b847fce631cca61a1e9c3de40
egm00033 <egm00033@red.ujaen.es>
2017-05-18 18:27:46 +0200

    limpiando código y ordenando

--- OpenCL_01/OpenCL/OpenCL.cpp
+++ OpenCL_01/OpenCL/OpenCL.cpp
@@ -7,7 +7,7 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int M = 60;
+	int M = 12;
 	int s = 2;
 	int LIST_SIZE = M*M;// Lista de elementos de tamaño M
 	// Vectores de entrada	
@@ -82,9 +82,9 @@ int _tmain(int argc, _TCHAR* argv[])
 		for (subS = 2; subS <= s; subS++)
 		{
 			double a=(double)s/(double)subS-s/subS;
-			printf("comparacion sub %i dif %f",subS,a);
+			printf("comparacion sub %i dif %f\n",subS,a);
 			if((double)s/(double)subS-s/subS==0){
-			break;
+				break;
 			}
 		}
 		int subM=M;
@@ -96,46 +96,46 @@ int _tmain(int argc, _TCHAR* argv[])
 			imagenm[i] = entrada[i];
 			sumn[i]=0;
 		}
-
+		size_t global_item_size=0;
 		do{//bucle iterativo para un S
 			if(swap){
-				printf("bucle \n");
+				printf("no borro1 \n");
 				for(int i = 0; i < LIST_SIZE; i++) {
 					imagenM[i] = maximos[i];
 					imagenm[i] = minimos[i];
 					//sumn[i]=0;
 				}
-				
-				while(subM%subS!=0&&subS<=subM/2){
+
+				while((subM%subS!=0&&subS<=subM/2)||(subM/2)%subS!=0){
 					printf("M=%i s= %i => %i\n",subM,subS,subS+1);
 					subS+=1;
 
 				}
 				if(subS>subM/2){
-					printf("analizar\n");
+					printf("no borro\n");
 					swap=false;
 					break;
 				}
 				//subM=subM/2;
-				
+
 			}
 
 			swap=true;
 			printf("iniciando bucle M=%i s= %i\n",subM,subS);
 			size_t local_item_size = subS*subS; // Grupo de trabajo de tamaño sxs
 
-			size_t global_item_size = subListTam/local_item_size; 
+			global_item_size = subListTam/local_item_size; 
 
 			printf("subListTam %i, global_item_size %i, local_item_size %i\n",subListTam,global_item_size,local_item_size);
 
 			// Creando el contexto de OpenCL
 			cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			// Crear lista de operadiones para ejecutar
 			//por defecto en orden | CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE (4º argumento)
 			cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			//crear buffers de memoria en el dispositivo por cada vector
 			cl_mem imagen_mem_objM = clCreateBuffer(context, CL_MEM_READ_ONLY, 
@@ -152,18 +152,15 @@ int _tmain(int argc, _TCHAR* argv[])
 			// Copiar cada vector de entrada en su buffer
 			ret = clEnqueueWriteBuffer(command_queue, imagen_mem_objM, CL_TRUE, 0,
 				LIST_SIZE * sizeof(int), imagenM, 0, NULL, NULL);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 			ret = clEnqueueWriteBuffer(command_queue, imagen_mem_objm, CL_TRUE, 0,
 				LIST_SIZE * sizeof(int), imagenm, 0, NULL, NULL);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
-
-
-			printf("antes de la construccion\n");
 			// Crear programa del shader
 			cl_program program = clCreateProgramWithSource(context, 1, 
 				(const char **)&shader, (const size_t *)&source_size, &ret);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			// compilar programa
 			ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
@@ -193,24 +190,23 @@ int _tmain(int argc, _TCHAR* argv[])
 				}
 			}
 
-			printf("Después de la construccion\n");
 			// Crear kernel de OpenCL
 			cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			// Establecer argumentos
 			ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&imagen_mem_objM);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 			ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&imagen_mem_objm);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&max_mem_obj);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			ret = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void *)&min_mem_obj);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 			ret = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *)&n_mem_obj);
-			printf("respuesta de la linea %d es %d\n", __LINE__, ret);
+			if(ret!=0)printf("respuesta de la linea %d es %d\n", __LINE__, ret);
 
 			//añadir para arreglar los problemas de salida
 			//ret = clSetKernelArg(kernel, 3, sizeof(int), &LIST_SIZE);
@@ -220,8 +216,8 @@ int _tmain(int argc, _TCHAR* argv[])
 				printf("Excedido el numero de global_work_size, debe ser menor que CL_DEVICE_ADDRESS_BITS\n");
 			}else if(local_item_size>=CL_DEVICE_MAX_WORK_GROUP_SIZE){
 				printf("Excedido el numero de local_item_size, debe ser menor que CL_DEVICE_MAX_WORK_GROUP_SIZE\n");
-			//}else if(global_item_size%local_item_size!=0){
-			//	printf("global_item_size %% local_item_size = %i %% %i   = %i\n",global_item_size,local_item_size,global_item_size%local_item_size);
+				//}else if(global_item_size%local_item_size!=0){
+				//	printf("global_item_size %% local_item_size = %i %% %i   = %i\n",global_item_size,local_item_size,global_item_size%local_item_size);
 			}else{
 				ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
 					&global_item_size, &local_item_size, 0, NULL, NULL);
@@ -229,13 +225,10 @@ int _tmain(int argc, _TCHAR* argv[])
 				// Copiar el buffer minimos en el vector minimos
 				ret = clEnqueueReadBuffer(command_queue, max_mem_obj, CL_TRUE, 0, 
 					LIST_SIZE * sizeof(int), maximos, 0, NULL, NULL);
-				printf("maximos ok\n");
 				ret = clEnqueueReadBuffer(command_queue, min_mem_obj, CL_TRUE, 0, 
 					LIST_SIZE * sizeof(int), minimos, 0, NULL, NULL);
-				printf("minimos ok\n");
 				ret = clEnqueueReadBuffer(command_queue, n_mem_obj, CL_TRUE, 0, 
 					LIST_SIZE * sizeof(double), sumn, 0, NULL, NULL);
-				printf("suma ok\n");
 
 
 
@@ -243,7 +236,7 @@ int _tmain(int argc, _TCHAR* argv[])
 				for(int i = 0; i < global_item_size; i++){
 					printf("%d. max = %d. min = %d. n= %f\n", imagenM[i], maximos[i], minimos[i], sumn[i]);
 				}
-				
+
 
 			}
 			// Clean up
@@ -263,9 +256,8 @@ int _tmain(int argc, _TCHAR* argv[])
 				printf(" M=%i s= %i",M,s);
 				printf(" > sM=%i ss= %i\n",subM,subS);
 				swap=false;
-				printf("condicion fin bucle\n");
 			}else{
-			//parametros para la siguiente ejecución del bucle
+				//parametros para la siguiente ejecución del bucle
 				subListTam=pow((double)(subM/subS),2);
 				subM=subM/subS;
 			}
@@ -273,15 +265,15 @@ int _tmain(int argc, _TCHAR* argv[])
 		printf("fin bucle\n");
 		double N = 0;
 		int tam = pow((double)(M/s),(2));
-		for(int i = 0; i < subListTam; i++){
+		for(int i = 0; i < global_item_size; i++){
 			N+=sumn[i];
-			//printf("%d. max = %d. min = %d. n= %f\n", imagenM[i], maximos[i], minimos[i], sumn[i]);
+			printf("%d. max = %d. min = %d. n= %f\n", imagenM[i], maximos[i], minimos[i], sumn[i]);
 		}
-		N=N/(double)(subListTam);
-		printf("Mostrado\n");
+		printf("subM/subS=subListTam %i/%i=%i\n",subM,subS,global_item_size);
+		N=N/(double)(global_item_size);
 		printf("M = %i \t S = %i \t N = %f\n",M,s,N);
 		system("pause");
-
+		printf("fin bucle\n\n\n");
 		s+=1;
 	}
 	free(maximos);
